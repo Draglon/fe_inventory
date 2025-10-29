84cf2e30306467df4763c38ee1723b439a49fc92
Draglon <s.nazarchuck@gmail.com>
2025-10-29 13:50:29 +0200

    Add web socket url

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,3 +1,3 @@
 import { io } from "socket.io-client";
 
-export default io(`ws://${process.env.NEXT_PUBLIC_API_URL}`);
+export default io(process.env.NEXT_PUBLIC_WS_URL || "ws://localhost:4004");
--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -3,7 +3,6 @@ import GuestLayout from "@/views/layouts/GuestLayout";
 import LoginForm from "@/views/Login/Form";
 
 const Login = () => {
-  console.log("process.env: ", process.env.NEXT_PUBLIC_APP_URL);
   return (
     <GuestLayout>
       <LoginForm />
