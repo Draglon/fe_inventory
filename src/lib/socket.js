c420efca910fcae5c450295c6b0ebf271464b873
Draglon <s.nazarchuck@gmail.com>
2025-10-29 15:55:51 +0200

    fix url

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -2,4 +2,4 @@ import { io } from "socket.io-client";
 
 // const WS_URL = process.env.NEXT_PUBLIC_WS_URL || "ws://localhost:4004"
 
-export default io("ws://be-inventory-1afa746d2cec.herokuapp.com");
+export default io("https://be-inventory-1afa746d2cec.herokuapp.com");
