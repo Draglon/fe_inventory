e583d6300a8884e3e78520a42c241a3b0c3ad05c
Draglon <s.nazarchuck@gmail.com>
2025-10-29 13:39:04 +0200

    process.env

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -3,6 +3,7 @@ import GuestLayout from "@/views/layouts/GuestLayout";
 import LoginForm from "@/views/Login/Form";
 
 const Login = () => {
+  console.log("process.env: ", process.env.NEXT_PUBLIC_APP_URL);
   return (
     <GuestLayout>
       <LoginForm />
